4944207ca65776fa765fef2df4feb8812ceacdd1
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-07-11 21:51:32 +0545

    Implementing mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,13 +4,15 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
-
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.use(cors({
-  credentials: true
-}));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 app.use(compression());
 app.use(cookieParser());
@@ -19,6 +21,12 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 const port = 3000;
-server.listen (port, () => {
-  console.log(`Server running on port-${port}`)
-})
+server.listen(port, () => {
+  console.log(`Server running on port-${port}`);
+});
+
+const MONGO_URL = `mongodb+srv://sudeepgautam33333:0nAenLOTlqIXeUli@bus-ticketing-system-fu.b5f91rx.mongodb.net/?retryWrites=true&w=majority&appName=Bus-ticketing-system-Full-Stack`;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
