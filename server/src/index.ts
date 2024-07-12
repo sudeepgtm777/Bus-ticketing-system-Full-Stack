0a5e39500a63abc4ed4bb254b197afae9d78f991
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-07-12 21:08:21 +0545

    Use of mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Server running on port-${port}`);
+  console.log(`Server running on http://localhost:${port}/`);
 });
 
 const MONGO_URL = `mongodb+srv://sudeepgautam33333:0nAenLOTlqIXeUli@bus-ticketing-system-fu.b5f91rx.mongodb.net/?retryWrites=true&w=majority&appName=Bus-ticketing-system-Full-Stack`;
